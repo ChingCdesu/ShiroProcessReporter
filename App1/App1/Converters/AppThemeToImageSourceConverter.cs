3651c6eb0b59d1d975dc1b73b47d4456fa389dd0
ChingCdesu <chingc.ccmail@gmail.com>
2024-05-07 02:35:58 +0800

    feat: add demo app for winui

---
+++ App1/App1/Converters/AppThemeToImageSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Converters
+{
+    public class AppThemeToImageSourceConverter : IValueConverter
+    {
+        public ImageSource LightImage { get; set; }
+        public ImageSource DarkImage { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (ApplicationTheme.Dark == value as ApplicationTheme?)
+            {
+                return DarkImage;
+            }
+
+            return LightImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App1/App1/Views/TrayIconView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using H.NotifyIcon;
+using H.NotifyIcon.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Views
+{
+    [ObservableObject]
+    public sealed partial class TrayIconView : UserControl
+    {
+        [ObservableProperty]
+        private bool _isWindowVisible;
+
+        [ObservableProperty]
+        private ApplicationTheme _theme;
+
+        public TrayIconView()
+        {
+            InitializeComponent();
+        }
+
+
+        [RelayCommand]
+        public void ShowHideWindow()
+        {
+            var window = App.MainWindow;
+            if (window == null)
+            {
+                return;
+            }
+
+            if (window.Visible)
+            {
+                window.Hide();
+            }
+            else
+            {
+                window.Show();
+            }
+            IsWindowVisible = window.Visible;
+        }
+
+        [RelayCommand]
+        public void ExitApplication()
+        {
+            this.TrayIcon.Dispose();
+            App.MainWindow?.Close();
+        }
+
+    }
+}
